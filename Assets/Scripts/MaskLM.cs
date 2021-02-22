620628e78216689df792b6c5f069a8015d871217
Roisin Bent <77437076+LIT-RoisinBent@users.noreply.github.com>
2021-02-22 14:37:07 +0000

    Initial commit

---
+++ Assets/Scripts/MaskLM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * The LevelManager from the Week 6 Lecture
+ */
+public class MaskLM : MonoBehaviour {
+
+    public static MaskLM instance;
+    public float gameTitleTime = 2f;
+    public Animator gameTitleAnim;
+    
+    private void Awake()
+    {
+        instance = this;
+        
+    }
+
+    void Start()
+    {
+        StartCoroutine("FadeTimer");
+    }
+
+    private IEnumerator FadeTimer()
+    {
+        yield return new WaitForSeconds(gameTitleTime);
+        gameTitleAnim.SetTrigger("Fade");
+    }
+
+}
